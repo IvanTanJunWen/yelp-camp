8a4b28aebb9841d34ebd6b0ff77e5b87cc435a85
IvanTann <tanjunwen93@gmail.com>
2020-05-26 21:16:05 +0800

    add mongo atlas

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var express     = require("express");
     User        = require("./models/user"),
     seedDB      = require("./seeds");
 
+
 //requiring routes    
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
@@ -18,7 +19,16 @@ var commentRoutes    = require("./routes/comments"),
    
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect("mongodb://localhost/yelp_camp_v12");
+// mongoose.connect("mongodb://localhost/yelp_camp_v12");
+mongoose.connect('mongodb+srv://root:P@ssw0rd123@cluster0-2hmyt.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log('Connected to DB!');
+}).catch(err => {
+    console.log('ERROR:', err.message);
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(methodOverride("_method"));
@@ -52,4 +62,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments" ,commentRoutes)
 
-app.listen(process.env.PORT, process.env.IP);
+app.listen(3000, () => {
+    console.log('server listening on port 3000');
+});
